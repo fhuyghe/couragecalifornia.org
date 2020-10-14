ad343ac080503bfe946564c4693ddff6791d5ee0
Francois Huyghe <francois@13milliseconds.com>
2020-10-14 16:03:04 -0400

    desktop header

--- web/app/themes/couragecalifornia2020/resources/views/partials/header.blade.php
+++ web/app/themes/couragecalifornia2020/resources/views/partials/header.blade.php
@@ -1,14 +1,16 @@
 <header class="top-banner">
   <div class="container">
-    <a class="brand" href="{{ home_url('/') }}">{{ get_bloginfo('name', 'display') }}</a>
+    <a class="brand" href="{{ home_url('/') }}">
+      <img src="@asset('images/cc-logo-white-500.png')" alt="Courage California logo" />
+    </a>
     <nav class="nav-primary">
       @if (has_nav_menu('primary_navigation'))
         {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']) !!}
       @endif
     </nav>
     <nav class="nav-secondary">
-      @if (has_nav_menu('social_menu'))
-        {!! wp_nav_menu(['theme_location' => 'social_menu', 'menu_class' => 'nav']) !!}
+      @if (has_nav_menu('secondary_navigation'))
+        {!! wp_nav_menu(['theme_location' => 'secondary_navigation', 'menu_class' => 'nav']) !!}
       @endif
     </nav>
   </div>
