901aef56029f382e33689b536d0718dce5aef582
Francois Huyghe <francois@13milliseconds.com>
2020-10-15 10:33:25 -0400

    Press Releases page

--- web/app/themes/couragecalifornia2020/resources/views/partials/content-press-releases.blade.php
+++ web/app/themes/couragecalifornia2020/resources/views/partials/content-press-releases.blade.php
@@ -8,8 +8,8 @@
         @if($releases)
         @foreach ($releases as $release) 
             <div class="release">
-                <h3><a href="{{ get_the_permalink($release->ID) }}">{!! get_the_title($release->ID) !!}</a></h3>
                 <time class="updated" datetime="{{ get_post_time('c', true, $release) }}">{{ get_the_date('', $release) }}</time>
+                <h3><a href="{{ get_the_permalink($release->ID) }}">{!! get_the_title($release->ID) !!}</a></h3>
             </div>
         @endforeach
     @endif
