48d10989eb23f97e4d7111a95519be07944bf3c1
Francois Huyghe <francois@13milliseconds.com>
2020-10-15 10:53:34 -0400

    more pages into containers

--- web/app/themes/couragecalifornia2020/resources/views/404.blade.php
+++ web/app/themes/couragecalifornia2020/resources/views/404.blade.php
@@ -3,10 +3,12 @@
 @section('content')
   @include('partials.page-header')
 
-  @if (!have_posts())
-    <div class="alert alert-warning">
-      {{ __('Sorry, but the page you were trying to view does not exist.', 'sage') }}
-    </div>
-    {!! get_search_form(false) !!}
-  @endif
+  <div class="container">
+    @if (!have_posts())
+      <div class="alert alert-warning">
+        {{ __('Sorry, but the page you were trying to view does not exist.', 'sage') }}
+      </div>
+      {!! get_search_form(false) !!}
+    @endif
+  </div>
 @endsection
--- web/app/themes/couragecalifornia2020/resources/views/page.blade.php
+++ web/app/themes/couragecalifornia2020/resources/views/page.blade.php
@@ -2,7 +2,9 @@
 
 @section('content')
   @while(have_posts()) @php the_post() @endphp
-    @include('partials.page-header')
-    @include('partials.content-page')
+    <div class="container">
+      @include('partials.page-header')
+      @include('partials.content-page')
+    </div>
   @endwhile
 @endsection
