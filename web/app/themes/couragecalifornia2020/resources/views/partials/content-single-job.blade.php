779b4d8aca79b95392b208326da0d044e50ef42e
Francois Huyghe <francois@13milliseconds.com>
2020-10-27 16:12:56 -0400

    single job layout

---
+++ web/app/themes/couragecalifornia2020/resources/views/partials/content-single-job.blade.php
@@ -0,0 +1,19 @@
+<article @php post_class() @endphp>
+  <header>
+    <div class="container">
+      <ul class="post-categories">
+        <li>Job Opening</li>
+      </ul>
+      <h1 class="entry-title">{!! get_the_title() !!}</h1>
+      <time class="updated" datetime="{{ get_post_time('c', true) }}">{{ get_the_date() }}</time>
+    </div>
+  </header>
+  <div class="entry-content">
+      <div class="container">
+    @php the_content() @endphp
+      </div>
+  </div>
+  <footer>
+    {!! wp_link_pages(['echo' => 0, 'before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']) !!}
+  </footer>
+</article>
