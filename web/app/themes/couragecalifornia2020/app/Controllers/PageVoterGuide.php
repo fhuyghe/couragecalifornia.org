87b86f133ad783a021adb421c2018f2d8d545c50
Francois Huyghe <francois@13milliseconds.com>
2020-12-07 12:18:17 -0500

    Voter guide desktop page

--- web/app/themes/couragecalifornia2020/app/Controllers/PageVoterGuide.php
+++ web/app/themes/couragecalifornia2020/app/Controllers/PageVoterGuide.php
@@ -10,8 +10,13 @@ class PageVoterGuide extends Controller
     public function data(){
         $data = [];
 		$data['top_illustration'] = get_field('top_illustration');
-		$data['bottom_illustration'] = get_field('bottom_illustration');
+		$data['intro'] = get_field('intro');
+		$data['intro_paragraph'] = get_field('intro_paragraph');
 		$data['blue'] = get_field('blue');
+		$data['partners'] = get_field('partners');
+		$data['equity'] = get_field('equity_on_trial');
+		$data['institute'] = get_field('courage_california_institute');
+		$data['bottom_illustration'] = get_field('bottom_illustration');
 
         return $data;
     }
--- web/app/themes/couragecalifornia2020/resources/views/partials/content-voter-guide.blade.php
+++ web/app/themes/couragecalifornia2020/resources/views/partials/content-voter-guide.blade.php
@@ -1,17 +1,25 @@
-<section id="intro">
+<section id="top" class="text-center">
     <div class="container">
         <div class="container">
-            <div class="row">
-                <div class="col-md-6 text">
-                    <h1>{!! App::title() !!}</h1>
-                    {!! the_field('intro') !!}
-                    <a class="button" href="https://progressivevotersguide.com/california">Voter Guide Website</a>
-                </div>
-                <div class="col-md-6 illustration">
-                    @if($data['top_illustration'])
-                        <img src="{{ $data['top_illustration']['url'] }}" />
-                    @endif
-                </div>
+            <h1>{!! App::title() !!}</h1>
+            {!! $data['intro'] !!}
+            <a class="button" href="https://progressivevotersguide.com/california">Voter Guide Website</a>
+
+            @if($data['top_illustration'])
+                <img src="{{ $data['top_illustration']['url'] }}" />
+            @endif
+        </div>
+    </div>
+</section>
+
+<section id="intro">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 subtitle">
+                <h3>{{ $data['intro_paragraph'] }}</h3>
+            </div>
+            <div class="col-md-6 text">
+                @php the_content() @endphp
             </div>
         </div>
     </div>
@@ -33,11 +41,45 @@
     </div>
 </section>
 
-<section id="content">
-    <div class="container">
-        @php the_content() @endphp
-    </div>
-    @if($data['bottom_illustration'])
-        <img src="{{ $data['bottom_illustration']['url'] }}" />
-    @endif
-</section>
+@if($data['partners'])
+    @php $section = $data['partners'] @endphp
+    <section id="partners">
+        <div class="container">
+            <h2>{{ $section['title'] }}</h2>
+            <p>{{ $section['text'] }}</p>
+            <div class="list">
+                {!! $section['partners'] !!}
+            </div>
+        </div>
+    </section>
+@endif
+
+@if($data['equity'])
+    @php $section = $data['equity'] @endphp
+    <section id="equity">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 text">
+                    <h2>{{ $section['title'] }}</h2>
+                    {!! $section['text'] !!}
+                </div>
+                <div class="col-md-6 image">
+                    <img src="{{ $section['image']['url'] }}" alt="{{ $section['image']['alt'] }}" />
+                </div>
+            </div>
+        </div>
+    </section>
+@endif
+
+@if($data['institute'])
+    @php $section = $data['institute'] @endphp
+    <section id="bottom" class="text-center">
+        <div class="container">
+            <h2>{{ $section['title'] }}</h2>
+            {!! $section['text'] !!}
+        </div>
+        @if($data['bottom_illustration'])
+            <img src="{{ $data['bottom_illustration']['url'] }}" />
+        @endif
+    </section>
+@endif
