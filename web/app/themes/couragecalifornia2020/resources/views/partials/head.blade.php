f406f499fa2f21d27c83713db699a5c4c1fc7d7a
Francois Huyghe <francois@13milliseconds.com>
2021-01-04 16:56:37 -0500

    favicon

--- web/app/themes/couragecalifornia2020/resources/views/partials/head.blade.php
+++ web/app/themes/couragecalifornia2020/resources/views/partials/head.blade.php
@@ -10,6 +10,25 @@
   {{-- Google Font --}}
   <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,400;0,700;1,400&display=swap" rel="stylesheet">
 
+  {{-- Favicon --}}
+  <link rel="apple-touch-icon" sizes="57x57" href="@asset('images/favicon/apple-icon-57x57.png')">
+  <link rel="apple-touch-icon" sizes="60x60" href="@asset('images/favicon/apple-icon-60x60.png')">
+  <link rel="apple-touch-icon" sizes="72x72" href="@asset('images/favicon/apple-icon-72x72.png')">
+  <link rel="apple-touch-icon" sizes="76x76" href="@asset('images/favicon/apple-icon-76x76.png')">
+  <link rel="apple-touch-icon" sizes="114x114" href="@asset('images/favicon/apple-icon-114x114.png')">
+  <link rel="apple-touch-icon" sizes="120x120" href="@asset('images/favicon/apple-icon-120x120.png')">
+  <link rel="apple-touch-icon" sizes="144x144" href="@asset('images/favicon/apple-icon-144x144.png')">
+  <link rel="apple-touch-icon" sizes="152x152" href="@asset('images/favicon/apple-icon-152x152.png')">
+  <link rel="apple-touch-icon" sizes="180x180" href="@asset('images/favicon/apple-icon-180x180.png')">
+  <link rel="icon" type="image/png" sizes="192x192"  href="@asset('images/favicon/android-icon-192x192.png')">
+  <link rel="icon" type="image/png" sizes="32x32" href="@asset('images/favicon/favicon-32x32.png')">
+  <link rel="icon" type="image/png" sizes="96x96" href="@asset('images/favicon/favicon-96x96.png')">
+  <link rel="icon" type="image/png" sizes="16x16" href="@asset('images/favicon/favicon-16x16.png')">
+  <link rel="manifest" href="@asset('images/favicon/manifest.json')">
+  <meta name="msapplication-TileColor" content="#000000">
+  <meta name="msapplication-TileImage" content="@asset('images/favicon/ms-icon-144x144.png')">
+  <meta name="theme-color" content="#000000">
+
   <!-- Google Tag Manager -->
   <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
   new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
