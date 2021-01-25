b255c34c5c02918d3f85e5f2c68beb80bb1a2b39
Francois Huyghe <francois@13milliseconds.com>
2021-01-25 09:50:45 -0500

    Add link to In The News

--- web/app/themes/couragecalifornia2020/resources/views/partials/content-single.blade.php
+++ web/app/themes/couragecalifornia2020/resources/views/partials/content-single.blade.php
@@ -5,14 +5,20 @@
       <h1 class="entry-title">{!! get_the_title() !!}</h1>
       @include('partials/entry-meta')
       {!! social_warfare() !!}
-      <div class="hero">
-      {!! get_the_post_thumbnail(get_the_ID(), 'large') !!}
-      </div>
+
+      @php $thumbnail = get_the_post_thumbnail(get_the_ID(), 'large') @endphp
+      @if($thumbnail)
+        <div class="hero">
+        {!! $thumbnail !!}
+        </div>
+      @endif
+
     </div>
   </header>
   <div class="entry-content">
       <div class="container">
     @php the_content() @endphp
+        {{-- ACTION --}}
         @if(in_category(7))
         <div>
           @php $link = get_post_meta(get_the_ID() , 'wpcf-field_ak_link_url', true) @endphp
@@ -20,6 +26,14 @@
               @php $link = get_field('link', get_the_ID()) @endphp
           @endif
           <a href="{{ $link }}" target="_blank" class="button yellow arrow">Act Now</a>
+        </div>
+        @endif
+
+        {{-- IN THE NEWS --}}
+        @if(in_category(6))
+        <div>
+          @php $link = get_field('link', get_the_ID()) @endphp
+          <a href="{{ $link }}" target="_blank" class="button yellow arrow">Read Now</a>
         </div>
         @endif
       </div>
