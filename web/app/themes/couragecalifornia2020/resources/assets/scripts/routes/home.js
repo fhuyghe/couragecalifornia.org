970bfd309cf7ea598e3d38c2ef25ddc721884051
Francois Huyghe <francois@13milliseconds.com>
2022-08-16 11:10:15 -0400

    add parallax back

--- web/app/themes/couragecalifornia2020/resources/assets/scripts/routes/home.js
+++ web/app/themes/couragecalifornia2020/resources/assets/scripts/routes/home.js
@@ -1,6 +1,7 @@
 import Isotope from 'isotope-layout'
 import ImagesLoaded from 'imagesloaded'
 import jQueryBridget from 'jquery-bridget';
+import 'jquery-parallax.js/parallax'
 
 jQueryBridget( 'isotope', Isotope, $ );
 jQueryBridget( 'imagesLoaded', ImagesLoaded, $ );
@@ -8,6 +9,7 @@ jQueryBridget( 'imagesLoaded', ImagesLoaded, $ );
 export default {
   init() {
     // JavaScript to be fired on the home page
+    $('.parallax-window').parallax({ imageSrc: '/path/to/image.jpg' });
     
     //Banner background
     $(window).on('scroll', function () { 
