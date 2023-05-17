93b04bf1464ef9cfbd8e1fd39db35907044a1656
Francois Huyghe <francois@13millieconds.com>
2023-05-17 13:54:31 -0400

    add DEBUG to staging

--- config/environments/staging.php
+++ config/environments/staging.php
@@ -14,4 +14,10 @@ use Roots\WPConfig\Config;
  * Example: `Config::define('DISALLOW_FILE_MODS', false);`
  */
 
+Config::define('WP_DEBUG', true);
+Config::define('WP_DEBUG_DISPLAY', true);
+Config::define('WP_DEBUG_LOG', env('WP_DEBUG_LOG') ?? true);
+Config::define('WP_DISABLE_FATAL_ERROR_HANDLER', true);
+Config::define('SCRIPT_DEBUG', true);
+
 Config::define('DISALLOW_INDEXING', true);
